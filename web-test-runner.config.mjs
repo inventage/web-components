2601495ac62e788a219fa10b2d5258df4f19f69a
Peter Siska <psiska@inventage.com>
2021-07-08 07:23:00 +0200

    Make sure tests run

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,21 +1,20 @@
 import { esbuildPlugin } from '@web/dev-server-esbuild';
 
 export default {
-  nodeResolve: {
-    exportConditions: ['browser', 'development']
-  },
+  files: 'packages/**/*.test.ts',
+  nodeResolve: true,
   concurrentBrowsers: 1,
   concurrency: 3,
-  coverageConfig: {
-    report: true,
-    reportDir: 'coverage',
-    threshold: {
-      statements: 85,
-      branches: 50,
-      functions: 80,
-      lines: 85,
-    },
-  },
+  // coverageConfig: {
+  //   report: true,
+  //   reportDir: 'coverage',
+  //   threshold: {
+  //     statements: 85,
+  //     branches: 50,
+  //     functions: 80,
+  //     lines: 85,
+  //   },
+  // },
   // testFramework: {
   //   config: {
   //     timeout: 100000,
