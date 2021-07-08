bb0e436a893483d1244c0c5abacf22e1dab032af
Peter Siska <psiska@inventage.com>
2021-07-08 11:08:01 +0200

    Add esbuild wtr example

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,26 +1,4 @@
-// import { esbuildPlugin } from '@web/dev-server-esbuild';
-
 export default {
   files: 'packages/**/*.test.js',
   nodeResolve: true,
-  concurrentBrowsers: 1,
-  concurrency: 3,
-  coverageConfig: {
-    report: true,
-    reportDir: 'coverage',
-    threshold: {
-      statements: 85,
-      branches: 50,
-      functions: 80,
-      lines: 85,
-    },
-  },
-  // testFramework: {
-  //   config: {
-  //     timeout: 100000,
-  //   },
-  // },
-  // plugins: [
-  //   esbuildPlugin({ ts: true })
-  // ],
 };
