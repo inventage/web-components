ced16365433f76bdc0ec3e8f021a9fe73bd0df65
Peter Siska <psiska@inventage.com>
2021-07-08 21:40:45 +0200

    Add test watch script

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,4 +1,12 @@
+// import fs from 'fs';
+//
+// const packages = fs.readdirSync('packages').filter(dir => fs.statSync(`packages/${dir}`).isDirectory());
+
 export default {
+  // groups: packages.map(pkg => ({
+  //   name: pkg,
+  //   files: `packages/${pkg}/**/*.test.js`,
+  // })),
   files: 'packages/**/*.test.js',
   nodeResolve: true,
 };
