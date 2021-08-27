e00e07adfe932798f1627c8e7515599bead6c8c7
Peter Siska <psiska@inventage.com>
2021-08-27 16:09:31 +0200

    chore: cleanup unused interface

--- .storybook/types.d.ts
+++
@@ -1,7 +0,0 @@
-import { TemplateResult } from '@inventage-web-components/common';
-
-interface Story<T> {
-  (args: T): TemplateResult;
-  args?: Partial<T>;
-  argTypes?: Record<string, unknown>;
-}
