2f594cb30f8111dffac3d9a339c764d5b6040d14
Peter Siska <psiska@inventage.com>
2021-10-18 14:52:47 +0200

    test: outsource some helpers into their own file

--- packages/portal-navigation/test/PortalNavigation.test.ts
+++ packages/portal-navigation/test/PortalNavigation.test.ts
@@ -6,6 +6,7 @@ import '../src/portal-navigation.js';
 import { PortalNavigation, NavigationEventListeners } from '../src/PortalNavigation.js';
 import { ConfigurationData, MenuLabel } from '../src/Configuration.js';
 import dataJson from '../data/test-data.json';
+import { handlerSpy, isInViewport } from './test-utils.js';
 
 const configurationData = dataJson as ConfigurationData;
 
@@ -14,52 +15,6 @@ const TEST_DATA_JSON_PATH = './data/test-data.json!';
 type WaitUntilOptions = {
   interval?: number;
   timeout?: number;
-};
-
-// From pwa-helpers
-// @see https://github.com/Polymer/pwa-helpers/blob/master/src/router.ts
-const handlerSpy = spy(e => {
-  if (e.defaultPrevented || e.button !== 0 || e.metaKey || e.ctrlKey || e.shiftKey) {
-    return;
-  }
-
-  const anchor = e.composedPath().filter((n: HTMLElement) => n.tagName === 'A')[0] as HTMLAnchorElement | undefined;
-  if (!anchor || (anchor.target && anchor.target !== '_self') || anchor.hasAttribute('download') || anchor.getAttribute('rel') === 'external') {
-    return;
-  }
-
-  const href = anchor.href;
-  if (!href || href.indexOf('mailto:') !== -1) {
-    return;
-  }
-
-  const location = window.location;
-  const origin = location.origin || location.protocol + '//' + location.host;
-
-  if (href.indexOf(origin) !== 0) {
-    return;
-  }
-
-  e.preventDefault();
-
-  if (href !== location.href) {
-    window.history.replaceState({}, '', href);
-  }
-});
-
-/**
- * Checks whether an element is in viewport
- *
- * @param element
- */
-const isInViewport = (element: Element) => {
-  const rect = element.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
 };
 
 /**
---
+++ packages/portal-navigation/test/test-utils.ts
@@ -0,0 +1,50 @@
+import { spy } from 'sinon';
+
+/**
+ * From pwa-helpers
+ *
+ * @see https://github.com/Polymer/pwa-helpers/blob/master/src/router.ts
+ */
+export const handlerSpy = spy(e => {
+  if (e.defaultPrevented || e.button !== 0 || e.metaKey || e.ctrlKey || e.shiftKey) {
+    return;
+  }
+
+  const anchor = e.composedPath().filter((n: HTMLElement) => n.tagName === 'A')[0] as HTMLAnchorElement | undefined;
+  if (!anchor || (anchor.target && anchor.target !== '_self') || anchor.hasAttribute('download') || anchor.getAttribute('rel') === 'external') {
+    return;
+  }
+
+  const href = anchor.href;
+  if (!href || href.indexOf('mailto:') !== -1) {
+    return;
+  }
+
+  const location = window.location;
+  const origin = location.origin || location.protocol + '//' + location.host;
+
+  if (href.indexOf(origin) !== 0) {
+    return;
+  }
+
+  e.preventDefault();
+
+  if (href !== location.href) {
+    window.history.replaceState({}, '', href);
+  }
+});
+
+/**
+ * Checks whether an element is in viewport
+ *
+ * @param element
+ */
+export const isInViewport = (element: Element) => {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
