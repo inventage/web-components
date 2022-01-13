1de94021d7703ceadb29d9ea525c6203c628364c
Peter Siska <63866+peschee@users.noreply.github.com>
2022-01-13 15:45:36 +0100

    build: remove publish script, relying on changeset actions output

--- scripts/publish.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Workaround for steps.changesets.outputs.published == 'true' which does not seem to work…
-# https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-environment-variable
-echo "package_published_on_npm=1" >> "$GITHUB_ENV"
-npx changeset publish
-
