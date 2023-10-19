fd58cac0a7472ad7865ead016d73337e14eba52b
Peter Siska <peter.siska@inventage.com>
2023-10-19 17:34:28 +0200

    chore: open by default

--- web-dev-server-storybook.config.js
+++ web-dev-server-storybook.config.js
@@ -7,7 +7,7 @@ import { rewriteDataJsonPaths } from './web-test-runner.config.js';
 export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
   nodeResolve: true,
   watch: true,
-  // open: true,
+  open: true,
   mimeTypes: {
     '**/custom-elements.json': 'js',
   },
